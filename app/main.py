61cb364998162f9500a41b829579d54e6c4d3255
Rk <eu.blr2025@gmail.com>
2026-04-03 16:42:10 +0530

    Revert "Update."
    This reverts commit da6ae5993c7478090aed9589078dbebbb71fabbb.

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from app.routers import health, items, users
+from app.routers import health, items
 
 app = FastAPI(
     title="Sample API",
@@ -10,7 +10,6 @@ app = FastAPI(
 
 app.include_router(health.router, tags=["health"])
 app.include_router(items.router, prefix="/items", tags=["items"])
-app.include_router(users.router, prefix="/users", tags=["users"])
 
 
 @app.get("/")
